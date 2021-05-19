21a1c16c5109b0ce8feb3d5ebed6ca86cf27885f
TroyaAcademy <troyaacademic@gmail.com>
2021-05-19 12:23:48 +0700

    fixed view

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Auth::routes();
 Route::get('/', [App\Http\Controllers\PageController::class, 'home']);
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
